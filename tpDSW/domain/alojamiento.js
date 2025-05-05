d25cd1e4faa93958bce488a5b32daf94c0f49de1
NicolasAlvarez43 <pealvarez@frba.utn.edu.ar>
2025-05-05 10:42:22 -0300

    Algunos fixes primer entrega

--- tpDSW/domain/alojamiento.js
+++ tpDSW/domain/alojamiento.js
@@ -19,45 +19,34 @@ export class Alojamiento{
     constructor(anfitrion, nombre, direccion){
         this.anfitrion = anfitrion;
         this.nombre = nombre;
-        this.descripcion = "";
         this.precioPorNoche = 0;
-        this.moneda = Moneda.DOLAR_USA; //MONEDA DEFAULT
-        this.horarioCheckIn = "";
-        this.horarioCheckOut = "";
-        this.direccion =  new Direccion();  //Lo dejamos vacio se define al momento que hagamos la creación como lo parseamos
-        this.cantHuespedesMax = 5; //CANTIDAD DEFAULT QUE SE PUEDE SETEAR
-        this.caracteristicas = []
-        this.reservas = []
-        this.fotos = []
+        this.moneda = Moneda.DOLAR_USA;
+        this.direccion = new Direccion();
+        this.cantHuespedesMax = 5;
+        this.caracteristicas = [];
+        this.reservas = [];
+        this.fotos = [];
     }
+
     estasDisponibleEn(rangoDeFechas){
-        this.reservas.some(reserva => this.rangoFechaEstaEntre(rangoDeFechas, reserva.rangoFechas))
-    }
-
-    rangoFechaEstaEntre(rangoDeFechas1, rangoDeFechas2){
-        return rangoDeFechas1.seSuperponeCon(rangoDeFechas1)
-
-
-        fechaInicialCumple = this.fechaEstaEntre(rangoDeFechas1.fechaInicio, rangoDeFechas2.fechaFin, rangoDeFechas2.fechaFin)
-        fechaFinalCumple = this.fechaEstaEntre(rangoDeFechas1.fechaFin, rangoDeFechas2.fechaFin, rangoDeFechas2.fechaFin)
-        return fechaInicialCumple && fechaFinalCumple
-    }
-
-    fechaEstaEntre(fecha, fechaInicio, fechaFinal){
-        return fecha > fechaInicio && fecha < fechaFinal
+        return this.reservas.every(reserva => 
+            !reserva.rangoFechas.seSuperponeCon(rangoDeFechas)
+        );
     }
 
     tuPrecioEstaDentroDe(valorMinimo, valorMaximo){
-        return (valorMinimo < this.precioPorNoche && valorMaximo > this.precioPorNoche);
+        return this.precioPorNoche >= valorMinimo && this.precioPorNoche <= valorMaximo;
     }
+
     tenesCaracteristica(caracteristica){
         return this.caracteristicas.includes(caracteristica);
     }
+
     puedenAlojarse(cantHuespedes){
         return cantHuespedes <= this.cantHuespedesMax;
     }
-    getNombre(){return this.nombre}
-    getAnfitrion() {
-        return this.anfitrion
-    }
+
+    getNombre(){ return this.nombre; }
+
+    getAnfitrion(){ return this.anfitrion; }
 }
