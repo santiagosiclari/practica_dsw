65c39dd7656d1eda422172e21ab1f052b0cf646e
jccastelo <jcastelo@frba.utn.edu.ar>
2025-05-10 14:58:58 -0300

    update dominio con ids

--- tpDSW/domain/reserva.js
+++ tpDSW/domain/reserva.js
@@ -1,4 +1,5 @@
 export class Reserva{
+    id //Agrego id para reserva
     fechaAlta
     huespedReservador
     cantHuespedes
@@ -15,17 +16,27 @@ export class Reserva{
         this.estado = EstadoReserva.PENDIENTE;
         this.precioPorNoche = alojamiento.getPrecioPorNoche();
     }
+    setId(idNuevo){this.id = idNuevo}
     actualizarEstado(estadoReserva){
         this.estado = estadoReserva;
     }
     getAlojamientoNombre(){return this.alojamiento.getNombre()}
+    getCantHuespedes(){return this.cantHuespedes}
+    getEstado(){return this.estado}
+    getPrecioPorNoche(){return this.precioPorNoche}
     getAnfitrionAlojamiento(){return this.alojamiento.getAnfitrion()}
     getRangoFechaInicio(){return this.rangoFechas.getFechaInicio()}
     getRangoFechaFinal(){return this.rangoFechas.getFechaFin()}
+    getRangoFechaInicioString(){return this.rangoFechas.fechaInicioToString()}
+    getRangoFechaFinalString(){return this.rangoFechas.fechaFinToString()}
     getHuespedReservadorNombre(){return this.huespedReservador.getNombre()}
+    getHuespedId(){return this.huespedReservador.getId()}
     calcularDias(){
         const dias = this.rangoFechas.fechaFin - this.rangoFechas.fechaInicio
         return Math.ceil(dias / (1000 * 60 * 60 * 24));
+    }
+    mostrarRangoReserva() {
+        return this.rangoFechas.rangoToString()
     }
 }
 export class RangoFechas{
@@ -40,16 +51,22 @@ export class RangoFechas{
     seSuperponeCon(otroRango) {
         return this.fechaInicio < otroRango.fechaFin && this.fechaFin > otroRango.fechaInicio;
     }
+    fechaInicioToString() {
+        return this.fechaInicio.toDateString()
+    }
+    fechaFinToString() {
+        return this.fechaFin.toDateString()
+    }
 }
-class EstadoReserva{
+export class EstadoReserva{
     constructor(nombre){
         this.nombre = nombre
     }
 }
 
-const PENDIENTE = new EstadoReserva('PENDIENTE')
-const CONFIRMADA = new EstadoReserva('CONFIRMADA')
-const CANCELADA = new EstadoReserva('CANCELADA')
+export const PENDIENTE = new EstadoReserva('PENDIENTE')
+export const CONFIRMADA = new EstadoReserva('CONFIRMADA')
+export const CANCELADA = new EstadoReserva('CANCELADA')
 
 export class CambioEstadoReserva{
     fecha
